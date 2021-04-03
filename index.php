9b26c5378fe30df37b004ae8c9d1eebd1e09f72f
hquan08 <81370825+hquan08@users.noreply.github.com>
2021-04-03 11:08:05 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h2 style="text-align: center;">
+        <?php
+        // put your code here
+           $a = 5;
+           $b = 6;
+           echo "Hello World</br>";
+           echo "if this shows up and is readable, it means I have done the xampp host setup for php project in netbeans</br>";
+           echo "Hello World with my first ever use GitHub</br>";
+           echo "5 + 6 equals to ", $a + $b;
+        ?>
+        </h2>
+    </body>
+</html>
